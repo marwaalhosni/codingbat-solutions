a303329f160a68327ca85ba8699a19ea674b7d1a
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-28 20:41:38 +0400

    Add solution for string1-deFront

---
+++ String1/deFront.java
@@ -0,0 +1,19 @@
+public class DeFront {
+    public String deFront(String str) {
+        String result = "";
+
+        if (str.length() >= 1 && str.charAt(0) == 'a') {
+            result += 'a';
+        }
+
+        if (str.length() >= 2 && str.charAt(1) == 'b') {
+            result += 'b';
+        }
+
+        if (str.length() > 2) {
+            result += str.substring(2);
+        }
+
+        return result;
+    }
+}
