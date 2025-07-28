5bd2e098bc054ddcb5debb29295f7ab96513df39
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-28 20:37:56 +0400

    Add solution for string1-atFirst

---
+++ String1/atFirst.java
@@ -0,0 +1,7 @@
+public class AtFirst {
+    public String atFirst(String str) {
+        if (str.length() >= 2) return str.substring(0, 2);
+        if (str.length() == 1) return str + "@";
+        return "@@";
+    }
+}
