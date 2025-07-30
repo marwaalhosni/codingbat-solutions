7a36114a0bcc2021324bccc66642a031a87a457e
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-30 19:10:49 +0400

    Add cigarParty solution

---
+++ cigarParty.java
@@ -0,0 +1,8 @@
+public class Logic1 {
+    public boolean cigarParty(int cigars, boolean isWeekend) {
+        if (isWeekend) {
+            return cigars >= 40;
+        }
+        return cigars >= 40 && cigars <= 60;
+    }
+}
