80be0de21f887fd27672de46b263219e78e0cce4
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-30 19:28:14 +0400

    Add CigarParty solution inside logic1 folder

---
+++ warmup2/arrayCount9.java
@@ -0,0 +1,11 @@
+public class ArrayCount9 {
+    public int arrayCount9(int[] nums) {
+        int count = 0;
+        for (int num : nums) {
+            if (num == 9) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
