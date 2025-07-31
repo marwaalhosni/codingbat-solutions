f0c537c73329d490a17eaedaa1496ed6782967d9
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-31 23:36:42 +0400

    Add recursion1 countPairs solution

---
+++ recursion1/countPairs.java
@@ -0,0 +1,8 @@
+public String countPairs(String str) {
+    if (str.length() < 3) return 0;
+    if (str.charAt(0) == str.charAt(2)) {
+        return 1 + countPairs(str.substring(1));
+    } else {
+        return countPairs(str.substring(1));
+    }
+}
