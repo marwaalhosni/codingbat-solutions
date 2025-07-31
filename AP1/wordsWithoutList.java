046310f49386db1033f9bcd231493be295a0cf9e
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-31 22:40:53 +0400

    Add solution for ap1-wordsWithoutList

---
+++ AP1/wordsWithoutList.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class WordsWithoutList {
+    public List<String> wordsWithoutList(String[] words, int len) {
+        List<String> result = new ArrayList<>();
+        for (String word : words) {
+            if (word.length() != len) {
+                result.add(word);
+            }
+        }
+        return result;
+    }
+}
