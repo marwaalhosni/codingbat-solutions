9a2f6c345073303fc579a6a2e14b53f73608eec6
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-31 23:01:56 +0400

    Add solution for recursion2-groupSum

---
+++ recursion2/groupSum.java
@@ -0,0 +1,8 @@
+public class Recursion2 {
+    public boolean groupSum(int start, int[] nums, int target) {
+        if (target == 0) return true;
+        if (start >= nums.length) return false;
+        if (groupSum(start + 1, nums, target - nums[start])) return true;
+        return groupSum(start + 1, nums, target);
+    }
+}
