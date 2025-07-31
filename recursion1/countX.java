31c62f68664c54708a0ca7fdcbd376731854b098
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-31 23:14:07 +0400

    Add solution for recursion1-countX

---
+++ recursion1/countX.java
@@ -0,0 +1,7 @@
+public class Recursion1 {
+    public int countX(String str) {
+        if (str.length() == 0) return 0;
+        int count = (str.charAt(0) == 'x') ? 1 : 0;
+        return count + countX(str.substring(1));
+    }
+}
