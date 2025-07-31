b22f241cf90f5ecef565f188062de2d3ddcb8760
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-31 23:16:10 +0400

    Add solution for recursion1-array11

---
+++ recursion1/array11.java
@@ -0,0 +1,7 @@
+public class Recursion1 {
+    public int array11(int[] nums, int index) {
+        if (index >= nums.length) return 0;
+        int count = (nums[index] == 11) ? 1 : 0;
+        return count + array11(nums, index + 1);
+    }
+}
