fc6b97cde294df2fa4880602302067ed8a574f2a
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-31 19:17:12 +0400

    Your commit message here

---
+++ array3/maxSpan.java
@@ -0,0 +1,13 @@
+public int maxSpan(int[] nums) {
+    int maxSpan = 0;
+    for (int i = 0; i < nums.length; i++) {
+        for (int j = nums.length - 1; j >= i; j--) {
+            if (nums[i] == nums[j]) {
+                int span = j - i + 1;
+                if (span > maxSpan) maxSpan = span;
+                break;
+            }
+        }
+    }
+    return maxSpan;
+}
