1888ad5272ee97d7e12f3af3ec569fb4bf0f4796
marwaalhosni <marwaalhosni17@gmail.com>
2025-07-31 23:04:44 +0400

    Add solution for recursion2-groupSum6

---
+++ recursion2/groupSum6.java
@@ -0,0 +1,8 @@
+public class Recursion2 {
+    public boolean groupSum6(int start, int[] nums, int target) {
+        if (start >= nums.length) return target == 0;
+        if (nums[start] == 6) return groupSum6(start + 1, nums, target - 6);
+        if (groupSum6(start + 1, nums, target - nums[start])) return true;
+        return groupSum6(start + 1, nums, target);
+    }
+}
