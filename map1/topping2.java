d4ab04f85de4bf7f2955a4c8c0e8a322a78592f5
marwaalhosni <marwaalhosni17@gmail.com>
2025-08-01 16:22:21 +0400

    Add map1 topping2 solution

---
+++ map1/topping2.java
@@ -0,0 +1,13 @@
+import java.util.Map;
+
+public class Map1Solutions {
+    public Map<String, String> topping2(Map<String, String> map) {
+        if (map.containsKey("ice cream")) {
+            map.put("yogurt", map.get("ice cream"));
+        }
+        if (map.containsKey("spinach")) {
+            map.put("spinach", "nuts");
+        }
+        return map;
+    }
+}
